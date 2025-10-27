ae8f0b23f49ec229d2c8d8b24c0d76db92867b3e
Miguel Barbeitos <mib@portugal.conseur.org>
2025-10-27 21:32:23 +0000

    atualização da estrutura

---
+++ Services/__init__.py
@@ -0,0 +1,4 @@
+echo '"""Pacote de serviços do chatbot da passagem de ano."""' > services/__init__.py
+git add services/__init__.py
+git commit -m "Adicionado __init__.py para corrigir imports"
+git push origin main
