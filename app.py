851c28df440ff71226e920da5a318b03ff6c9776
Miguel Barbeitos <mib@portugal.conseur.org>
2025-10-28 21:19:39 +0000

    atualizar estrategia com tratamento de emails de resposta

--- app.py
+++ app.py
@@ -576,7 +576,7 @@ def gerar_resposta(pergunta: str, perfil_completo: dict):
     )
 
     guardar_mensagem(perfil_completo["nome"], pergunta, resposta_llm, contexto="geral", perfil=perfil_completo)
-    return resposta_llm
+    return resposta_ll
 
 # =====================================================
 # 💬 INTERFACE STREAMLIT (CHAT)
